0c0291bc542cb3267df5ecd32016a0e2eb8a326f
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-04-28 09:19:54 +0700

    .

--- src/ManagerStudent/student.sql
+++ src/ManagerStudent/student.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS list_students;
 CREATE DATABASE IF NOT EXISTS list_students;
-CREATE DATABASE list_students;
 
 use list_students;
-create table students(
+DROP TABLE IF EXISTS students;
+CREATE TABLE IF NOT EXISTS students (
     studentID varchar(6) primary key,
     name varchar(20),
     address varchar(50),
     phone int
-)
+);
 
-insert into students values (id, 'name', 'address', phone);
+insert into students values ('id', 'name', 'address', 'phone');
